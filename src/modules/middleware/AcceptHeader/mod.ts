1df353c67fa6341f92d98d748c06532be579a397
Eric Crooks <crookse@users.noreply.github.com>
2023-09-18 18:14:26 -0400

    refactor: make AcceptHeader middleware tests match ETag middleware tests

--- src/modules/middleware/AcceptHeader/mod.ts
+++ src/modules/middleware/AcceptHeader/mod.ts
@@ -3,87 +3,131 @@ import { HTTPError } from "../../../core/errors/HTTPError.ts";
 import { Middleware } from "../../../standard/http/Middleware.ts";
 
 type Options = {
-  fail_on_accept_header_mismatch: boolean;
+  /** Throw if the response's Content-Type header does not match the request's Accept header? */
+  throw_if_accept_header_mismatched?: boolean;
+  /** Throw if the request's Accept header is missing? */
+  throw_if_accept_header_missing?: boolean;
+};
+
+type Context = {
+  request: Request;
+  response: Response;
+  /** A flag each handler function can use to see if it should or should not process the context further. */
+  done?: boolean;
 };
 
 const defaultOptions: Options = {
-  fail_on_accept_header_mismatch: false,
+  throw_if_accept_header_mismatched: true,
+  throw_if_accept_header_missing: true,
 };
 
-export class AcceptHeader extends Middleware {
-  #options: Options = defaultOptions;
+class AcceptHeaderMiddleware extends Middleware {
+  #options: Options;
 
-  constructor(options: Options) {
+  /**
+   * Construct the middleware that handles Accept headers.
+   *
+   * @param options (Optional) Options to control the middleware's behavior. See
+   * {@link Options} for details.
+   */
+  constructor(options: Options = defaultOptions) {
     super();
+
+    // TODO(crookse) Check if the options are correct before setting them
     this.#options = {
       ...defaultOptions,
       ...options,
     };
   }
 
-  public async ALL(request: Request) {
-    const response = super.next<Response>(request);
+  public ALL(request: Request) {
+    return Promise
+      .resolve()
+      .then(() => this.handleIfAcceptHeaderMissing(request))
+      .then(() => super.next<Response>(request))
+      .then((response) => ({ request, response }))
+      .then((context) => this.handleHeaders(context))
+      .then((context) => this.sendResponse(context));
+  }
 
-    if (!response) {
+  protected handleHeaders(context: Context) {
+    if (context.done) {
+      return context;
+    }
+
+    const reqHeader = context.request.headers.get("accept")?.toLowerCase();
+
+    // Request accepts anything so send it
+    if (reqHeader && reqHeader.includes("*/*")) {
+      context.done = true;
+      return context;
+    }
+
+    const resHeader = context.response.headers.get("content-type")
+      ?.toLowerCase();
+
+    if (!resHeader) {
       throw new HTTPError(
         Status.InternalServerError,
-        "The server was unable to generate a response",
+        "The server did not generate a response with a Content-Type header",
       );
     }
 
-    const result = this.#requestAcceptsResponse(request, response);
+    const [contentType, _charset] = resHeader.split(";");
 
-    if (!this.#options.fail_on_accept_header_mismatch) {
-      return response;
+    if (
+      (reqHeader !== resHeader) ||
+      !reqHeader.includes(contentType)
+    ) {
+      // Only throw if the option is enabled
+      if (this.#options.throw_if_accept_header_mismatched) {
+        throw new HTTPError(
+          Status.UnprocessableEntity,
+          "The server did not generate a response matching the request's Accept header",
+        );
+      }
     }
 
-    if (!request.headers.has("accept")) {
+    context.done = true;
+
+    return context;
+  }
+
+  protected handleIfAcceptHeaderMissing(request: Request) {
+    if (
+      this.#options.throw_if_accept_header_missing &&
+      !request.headers.get("accept")
+    ) {
       throw new HTTPError(
         Status.BadRequest,
-        `Request is missing 'Accept' header`,
+        `Accept header is required`,
       );
     }
-
-    if (!result) {
-      throw new HTTPError(
-        Status.UnprocessableEntity,
-        `The server cannot generate a response matching the value(s) in the request's 'Accept' header: ${
-          request.headers.get("accept")
-        }`,
-      );
-    }
-
-    return response;
   }
 
-  /**
-   * If the request Accept header is present, then make sure the response
-   * Content-Type header is accepted.
-   *
-   * @param request
-   * @param response
-   */
-  #requestAcceptsResponse(request: Request, response: Response): boolean {
-    const requestAcceptHeader = request.headers.get("accept");
-
-    if (!requestAcceptHeader) {
-      return false;
-    }
-
-    const responseContentTypeHeader = response.headers?.get("content-type");
-
-    if (!responseContentTypeHeader) {
-      return false;
-    }
-
-    if (requestAcceptHeader.includes("*/*")) {
-      return true;
-    }
-
-    if (requestAcceptHeader.includes(responseContentTypeHeader)) {
-      return true;
-    }
-
-    return false;
+  protected sendResponse(context: Context) {
+    return context.response;
   }
 }
+
+/**
+ * Get the middleware class that handles Accept headers.
+ *
+ * @param options (Optional) Options to control the middleware's behavior. See
+ * {@link Options} for details.
+ *
+ * @returns The middleware class that can be instantiated. When it is
+ * instantiated, it instantiates with the provided `options`. If no options are
+ * provided, it uses its default options.
+ */
+function AcceptHeader(options: Options = defaultOptions) {
+  return class DefaultAcceptHeader extends AcceptHeaderMiddleware {
+    constructor() {
+      super(options);
+    }
+  };
+}
+
+// FILE MARKER - PUBLIC API ////////////////////////////////////////////////////
+
+export { AcceptHeader, AcceptHeaderMiddleware, defaultOptions, type Options };
