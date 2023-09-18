4a94c9fd447e08a069e59074ae312a9ddd6edcb6
Eric Crooks <crookse@users.noreply.github.com>
2023-09-18 18:50:05 -0400

    chore: deno fmt

--- src/modules/builders/RequestBuilder.ts
+++ src/modules/builders/RequestBuilder.ts
@@ -1,42 +1,42 @@
 class RequestBuilder {
-    #request: RequestInit = {};
-    #path = "<path not provided>";
-  
-    path(path: string) {
-      this.#path = path;
-      return this;
-    }
-  
-    get() {
-      this.#request.method = "get";
-      return this;
-    }
-  
-    post() {
-      this.#request.method = "post";
-      return this;
-    }
-  
-    put() {
-      this.#request.method = "put";
-      return this;
-    }
-  
-    patch() {
-      this.#request.method = "patch";
-      return this;
-    }
-  
-    delete() {
-      this.#request.method = "delete";
-      return this;
-    }
-  
-    build() {
-      return new Request(this.#path, this.#request);
-    }
+  #request: RequestInit = {};
+  #path = "<path not provided>";
+
+  path(path: string) {
+    this.#path = path;
+    return this;
   }
 
-  export function request() {
-    return new RequestBuilder();
+  get() {
+    this.#request.method = "get";
+    return this;
   }
+
+  post() {
+    this.#request.method = "post";
+    return this;
+  }
+
+  put() {
+    this.#request.method = "put";
+    return this;
+  }
+
+  patch() {
+    this.#request.method = "patch";
+    return this;
+  }
+
+  delete() {
+    this.#request.method = "delete";
+    return this;
+  }
+
+  build() {
+    return new Request(this.#path, this.#request);
+  }
+}
+
+export function request() {
+  return new RequestBuilder();
+}
